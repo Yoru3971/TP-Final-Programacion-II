1c92a5c7b761ac4d00dbbdeceb83998705a8ca7c
Emiliano Daverio <emilianodaverio@gmail.com>
2024-11-11 21:54:44 -0300

    Modificacion a la clase habitacion
    Un cambio para poder manejar mejor la disponibilidad, todavia queda pendiente ver como manejar eso a la hora de hacer reservas

--- src/modelos/Habitacion.java
+++ src/modelos/Habitacion.java
@@ -1,65 +1,79 @@
 package modelos;
 
+import java.util.HashMap;
+import java.util.Objects;
 import enumeraciones.EstadoHabitacion;
 import enumeraciones.TipoHabitacion;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Objects;
-
 public class Habitacion {
     private Integer numeroHabitacion;
-    private HashMap<LocalDateTime,Boolean> disponibilidadReserva;       //<FECHA,DISPONIBILIDAD>
+    private HashMap<Integer, Boolean> disponibilidadReserva; // <día, disponibilidad>
     private EstadoHabitacion estadoActual;
     private TipoHabitacion tipoHabitacion;
     private Double precioDiario;
 
-
-    // VER SI AGREGAR ALGUN CONSTRUCTOR, SIN ALGUN ATRIBUTO EN PARTICULAR
-    public Habitacion(Integer numeroHabitacion, HashMap<LocalDateTime, Boolean> disponibilidadReserva, EstadoHabitacion estadoActual, TipoHabitacion tipoHabitacion, Double precioDiario) {
+    public Habitacion(Integer numeroHabitacion, EstadoHabitacion estadoActual, TipoHabitacion tipoHabitacion, Double precioDiario) {
         this.numeroHabitacion = numeroHabitacion;
-        this.disponibilidadReserva = disponibilidadReserva;
         this.estadoActual = estadoActual;
         this.tipoHabitacion = tipoHabitacion;
         this.precioDiario = precioDiario;
+        this.disponibilidadReserva = new HashMap<>();
+
+        // Inicializamos todos los días del 1 al 31 como disponibles (true)
+        for (int dia = 1; dia <= 31; dia++) {
+            disponibilidadReserva.put(dia, true);
+        }
     }
 
     public Integer getNumeroHabitacion() {
         return numeroHabitacion;
     }
+
     public void setNumeroHabitacion(Integer numeroHabitacion) {
         this.numeroHabitacion = numeroHabitacion;
     }
-    public HashMap<LocalDateTime, Boolean> getDisponibilidadReserva() {
-        return disponibilidadReserva;
-    }
-    public void setDisponibilidadReserva(HashMap<LocalDateTime, Boolean> disponibilidadReserva) {
-        this.disponibilidadReserva = disponibilidadReserva;
-    }
+
     public EstadoHabitacion getEstadoActual() {
         return estadoActual;
     }
+
     public void setEstadoActual(EstadoHabitacion estadoActual) {
         this.estadoActual = estadoActual;
     }
+
     public TipoHabitacion getTipoHabitacion() {
         return tipoHabitacion;
     }
+
     public void setTipoHabitacion(TipoHabitacion tipoHabitacion) {
         this.tipoHabitacion = tipoHabitacion;
     }
+
     public Double getPrecioDiario() {
         return precioDiario;
     }
+
     public void setPrecioDiario(Double precioDiario) {
         this.precioDiario = precioDiario;
+    }
+
+    public HashMap<Integer, Boolean> getDisponibilidadReserva() {
+        return disponibilidadReserva;
+    }
+
+    public void setDisponibilidadReserva(HashMap<Integer, Boolean> disponibilidadReserva) {
+        this.disponibilidadReserva = disponibilidadReserva;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Habitacion that)) return false;
-        return Objects.equals(numeroHabitacion, that.numeroHabitacion) && Objects.equals(disponibilidadReserva, that.disponibilidadReserva) && estadoActual == that.estadoActual && tipoHabitacion == that.tipoHabitacion && Objects.equals(precioDiario, that.precioDiario);
+        return Objects.equals(numeroHabitacion, that.numeroHabitacion) &&
+                Objects.equals(disponibilidadReserva, that.disponibilidadReserva) &&
+                estadoActual == that.estadoActual &&
+                tipoHabitacion == that.tipoHabitacion &&
+                Objects.equals(precioDiario, that.precioDiario);
     }
 
     @Override
@@ -72,7 +86,7 @@ public class Habitacion {
         return "Habitacion" + "\n" +
                 "Numero = " + numeroHabitacion + "\n" +
                 "Disponibilidad = " + disponibilidadReserva + "\n" +
-                "Estado Acutal = " + estadoActual + "\n" +
+                "Estado Actual = " + estadoActual + "\n" +
                 "Tipo de Habitacion = " + tipoHabitacion + "\n" +
                 "Precio Diario = " + precioDiario;
     }
